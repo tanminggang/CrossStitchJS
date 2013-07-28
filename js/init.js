e2669c06598654c6daf59499b1244aab2cb3104c
JenniferSpry <jennifer.spry@web.de>
2013-07-28 21:34:59 +0200

    added paper.js

---
+++ js/init.js
@@ -0,0 +1,8 @@
+var canvas = document.getElementById('stipat');
+var ctx = canvas.getContext('2d');
+
+var squarewidth = 10;
+var points = [
+		{position: [2,4], color: "red"},
+		{position: [3,4], color: "blue"}
+	];
